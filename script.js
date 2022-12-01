7582d43bcb2d8930c4dac106f119b03034194849
54padmakshi <115454581+54padmakshi@users.noreply.github.com>
2022-12-01 11:13:12 +0530

    updated

--- script.js
+++ script.js
@@ -29,39 +29,42 @@ function myFunction(){
 
       if (!loerro && !acto)
       {
-
-    
-
-      const l1 = document.getElementById("location").value;
-      const ac1= document.getElementById("acestoken").value;
- 
-      console.log(l1);
-      console.log(ac1);
+   
+        const l1 = document.getElementById("location").value;
+        const ac1= document.getElementById("acestoken").value;
       
-     const url = "http://api.weatherstack.com/current?access_key= "+ac1+ "&query="+l1+ "api.weatherstack.com/current?access_key= "+ac1+ "&query="+l1;
+ 
+      console.log(l1); //india
+      console.log(ac1); //0a70adfd730c940ac0bbd06c4e12152c      S5FWJZQKK43QRRUL9YQHTUABC-india
+      
+     const url = " https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/ "+l1+"?unitGroup=metric&key="+ac1 +"&contentType=json";
+      //  access key -  S5FWJZQKK43QRRUL9YQHTUABC
+     //const url = "http://api.weatherstack.com/current?access_key= "+ac1+ "&query="+l1+ "api.weatherstack.com/current?access_key= "+ac1+ "&query="+l1;
      console.log(url);
-     getapi(url);
-     async function getapi(url1) {
-        const response = await fetch(url1);
-        var data =await response.json();
+    // getapi(url);
+     async function getapi(url) {
+        const response = await fetch(url);
+        data =await response.json();
         console.log(data);
+       
         show(data);
      }
      function show(data) {
-        var loc = data.location.name+","+data.location.country;
-        var lat=data.location.lat;
-        var long = data.location.lon;
-        var re =data.location.region; //added extra
-        var tz = data.location.timezone_id;
-        var lt = data.location.localtime; //added extra
-        var lte =data.location.localtime_epoch; //added extra
-        var hm = data.current.humidity;
-        var ws = data.current.wind_speed;
-        var ps =data.current.pressure;
-        var wd = data.current.wind_dir;
-        var uv = data.current.uv_index;
-        var fe = data.current.feelslike;
-        var clo = data.current.cloudcover; //added extra
+        var loc = data.resolvedAddress;
+       var lat=data.latitude;
+       var long = data.longitude;
+        var re =data.address;
+        var tz = data.timezone;
+        var lt = data.description; 
+        var lte =data.currentConditions.datetimeEpoch; //added extra
+        var hm = data.currentConditions.humidity;
+        var ps =data.currentConditions.pressure;
+        var ws = data.currentConditions.windspeed;
+        
+        var wd = data.currentConditions.winddir;
+        var uv = data.currentConditions.uvindex;
+        var fe = data.currentConditions.feelslike;
+        var clo = data.currentConditions.cloudcover; //added extra    
 
 
         document.getElementById("loc").innerHTML +=loc;
@@ -72,17 +75,17 @@ function myFunction(){
         document.getElementById("lt").innerHTML +=lt;
         document.getElementById("lte").innerHTML +=lte;
         document.getElementById("hu").innerHTML +=hm;
-
-        document.getElementById("ws").innerHTML +=ws;
         document.getElementById("pr").innerHTML +=ps;
+       document.getElementById("ws").innerHTML +=ws;
+        
         document.getElementById("wd").innerHTML +=wd;
         document.getElementById("ui").innerHTML +=uv;
         document.getElementById("fl").innerHTML +=fe;
-        document.getElementById("ccover").innerHTML +=clo;
+        document.getElementById("ccover").innerHTML +=clo;    
         
      }
-     
+     getapi(url);
       
     }
-
+   
 }
